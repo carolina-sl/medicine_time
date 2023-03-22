423aca8d343f165ae2198ac681900ee3d5eb2c35
Carolina Serafim Lauffer <carolina.lauffer@gmail.com>
2023-03-21 23:33:32 -0300

    adicionado um input para o usuario adicionar um horario inicial e se fazer o calculo a partir desse horario digitado

--- app/src/views/dados.php
+++ app/src/views/dados.php
@@ -4,33 +4,31 @@
     require __DIR__ . '/../classes/Remedio.php';
     require __DIR__ . '/../classes/Request.php';
 
-
-    $diaInterio = 24;
+    $diaInteiro = 24;
 
     $nome = $_POST['nome'] ? $_POST['nome'] : 0;
     $remedio = $_POST['remedio'] ? $_POST['remedio'] : 0;
     $intervalo = $_POST['intervalo'] ? $_POST['intervalo'] : 0;
-
-    // construção dos horários:
-
-    //var_dump($_REQUEST);
-    // var_dump($_SERVER);
+    $horaInicial = $_POST['horario_inicial'] ? intval($_POST['horario_inicial']) : 0;
 
     $request = new Request();
 
-    echo "HORÁRIOS DO SEU REMÉDIO";
+    echo "<h2> HORÁRIOS DO SEU REMÉDIO </h2>";
     echo "<br><br>";
 
+    $horaAtualizada = $intervalo + $horaInicial;
+    
     switch ($intervalo) {
-
         case 2:
-            $resultado = $diaInterio / $intervalo;
+            $resultado = $diaInteiro / $intervalo;
+            $horaAtualizada = $intervalo + $horaInicial;
             echo " - $resultado AM  <br> - $resultado PM";
             break;
 
         case 4:     
-            $resultado = $diaInterio / $intervalo;
-            for ($x=0; $x < $diaInterio; $x = $x + 4) { 
+            $resultado = $diaInteiro / $intervalo;
+            for ($x=$horaInicial; $x < $diaInteiro; $x = $x + 4) { 
+                $horaAtualizada = $x + $intervalo;
                 if ($x >= 12) {
                     echo " <br> - PM $x"; 
                 } else {
@@ -41,8 +39,8 @@
             break;
 
         case 6:
-            $resultado = $diaInterio / $intervalo;
-            for ($x=0; $x < $diaInterio; $x = $x + 6) { 
+            $resultado = $diaInteiro / $intervalo;
+            for ($x=$horaInicial; $x < $diaInteiro; $x = $x + 6) { 
                 if ($x >= 12) {
                     echo " <br> - PM $x"; 
                 } else {
@@ -52,8 +50,8 @@
             echo "<br><br>Seu remédio é de $intervalo/$intervalo horas. ";
             break;
         case 8:
-            $resultado = $diaInterio / $intervalo;
-            for ($x=0; $x < $diaInterio; $x = $x + 8) { 
+            $resultado = $diaInteiro / $intervalo;
+            for ($x=$horaInicial; $x < $diaInteiro; $x = $x + 8) { 
                 if ($x >= 12) {
                     echo " <br> - PM $x"; 
                 } else {
@@ -63,8 +61,8 @@
             echo "<br><br>Seu remédio é de $intervalo/$intervalo horas. ";
             break;
             case 12:
-                $resultado = $diaInterio / $intervalo;
-                for ($x=0; $x < $diaInterio; $x = $x + 12) { 
+                $resultado = $diaInteiro / $intervalo;
+                for ($x=$horaInicial; $x < $diaInteiro; $x = $x + 12) { 
                     if ($x >= 12) {
                         echo " <br> - PM $x"; 
                     } else {
--- app/src/views/index.php
+++ app/src/views/index.php
@@ -1,6 +1,5 @@
 
 <?php
-
     require __DIR__ . '/../classes/Pessoa.php';
     require __DIR__ . '/../classes/Remedio.php';
     require __DIR__ . '/../classes/Request.php';
@@ -8,30 +7,22 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title> views </title>
-    
-    <form action="dados.php" method="POST">
-                
-                <strong>Nome:</strong> <input type="text" name="nome"> <br><br>
-                <strong>Remédio:</strong> <input type="text" name="remedio"> <br><br>
-                <strong>Intervalo (horas):</strong> <input type="text" name="intervalo"> <br><br>
-                <strong>horário:</strong> <input type="text" name="horario"> <br><br>
-                
-                <input type="submit" value="Enviar">
-                <br><br><br>
-                
-            </form>
-    
 </head>
 <body>
-    
+    <div>
+        <form action="dados.php" method="POST">
+            <strong>Nome:</strong> <input type="text" name="nome"> <br><br>
+            <strong>Remédio:</strong> <input type="text" name="remedio"> <br><br>
+            <strong>Intervalo (horas):</strong> <input type="text" name="intervalo"> <br><br>
+            <strong> Digite o horário inícial:</strong> <input type="time" name="horario_inicial" required> <br><br>
+            <input type="submit" value="Calcular">
+            <br><br><br>
+        </form>
+    </div>
 </body>
 </html>
