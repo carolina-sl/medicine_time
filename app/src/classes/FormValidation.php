4dad17862e37f9848587e586121ebf1b393479ca
Carolina Serafim Lauffer <carolina.lauffer@gmail.com>
2023-06-06 00:00:22 -0300

    alterado o texto do metodo stringValidate

--- app/src/classes/FormValidation.php
+++ app/src/classes/FormValidation.php
@@ -6,7 +6,7 @@ class FormValidation {
     public static function stringValidate($string) {
 
         if (strlen($string) <= 2 && $_SERVER['REQUEST_METHOD'] == 'POST') {
-            return "<p style='color:red;'>Seu nome deve conter pelo menos 2 caracteres </p>";
+            return "<p style='color:red;'>Esse campo deve conter pelo menos 2 caracteres </p>";
         }
     }
 
